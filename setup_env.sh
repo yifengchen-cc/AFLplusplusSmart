7a2365082082a4ccb7e6973cf5211e55c94d8200
yifengchen-cc <yifengchen66@gmail.com>
2020-06-28 09:50:12 +0800

    2.63c+aflsmart

--- setup_env.sh
+++ setup_env.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-#SCRIPT=$(readlink -f "$0")
-SCRIPT=$(readlink -f -- "$0")
+SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-export AFLPLUSPLUSSMART=$SCRIPTPATH
-export PATH=$PATH:$AFLPLUSPLUSSMART:$AFLPLUSPLUSSMART/peach-3.0.202-source/output/linux_x86_64_debug/bin
-export AFL_PATH=$AFLPLUSPLUSSMART
+export AFLSMART=$SCRIPTPATH
+export PATH=$PATH:$AFLSMART:$AFLSMART/peach-3.0.202-source/output/linux_x86_64_debug/bin
+export AFL_PATH=$AFLSMART
 export LD_LIBRARY_PATH=/usr/local/lib
+
